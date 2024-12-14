74fcdde9a38b4e24245eb04bf0a7260050293f30
kirmake <kirmake@gmail.com>
2024-12-14 09:59:15 +0200

    Added test for debugging

---
+++ UITests/Tests/Test.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using UITests.Utilities;
+
+namespace UITests.Tests
+{
+    [TestFixture]
+    public class TreasuryTest
+    {
+
+        //TODO fix issue
+#pragma warning disable NUnit1032 // An IDisposable field/property should be Disposed in a TearDown method
+        private IWebDriver _driver;
+#pragma warning restore NUnit1032 // An IDisposable field/property should be Disposed in a TearDown method
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = DriverFactory.CreateChromeDriver();
+        }
+
+        [Test]
+        public void TestTreasuryConnection()
+        {
+            try
+            {
+                _driver.Navigate().GoToUrl("https://treasury.coti.io/");
+            }
+            catch (Exception ex)
+            {
+
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
