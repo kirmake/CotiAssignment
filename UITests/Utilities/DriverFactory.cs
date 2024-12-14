a01e7d33ff830312f5a0926dd9a62b5253ba422d
kirmake <kirmake@gmail.com>
2024-12-14 09:51:55 +0200

    Created driver factory

---
+++ UITests/Utilities/DriverFactory.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace UITests.Utilities
+{
+    internal class DriverFactory
+    {
+        public static IWebDriver CreateChromeDriver()
+        {
+            var options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+            return new ChromeDriver(options);
+        }
+    }
+}
